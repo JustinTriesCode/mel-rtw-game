bf32b03609f14f82e7da413c193f528881eba5aa
Justin <c7l5l@students.cs.ubc.ca>
2026-02-13 19:57:20 -0800

    Added assets and loader file

---
+++ MelSimpleGame/src/ui/ResourceLoader.java
@@ -0,0 +1,31 @@
+package ui;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class ResourceLoader {
+
+    // Load an image from res/images
+    public static BufferedImage loadImage(String fileName) {
+        try {
+            return ImageIO.read(new File("res/images/" + fileName));
+        } catch (IOException e) {
+            System.out.println("Error: Could not find image " + fileName);
+            return null;
+        }
+    }
+
+    // Load a custom font from res/fonts
+    public static Font loadFont(String fileName, float size) {
+        try {
+            Font font = Font.createFont(Font.TRUETYPE_FONT, new File("res/fonts/" + fileName));
+            return font.deriveFont(size);
+        } catch (Exception e) {
+            System.out.println("Error: Could not load font. Using Arial instead.");
+            return new Font("Arial", Font.BOLD, (int)size);
+        }
+    }
+}
