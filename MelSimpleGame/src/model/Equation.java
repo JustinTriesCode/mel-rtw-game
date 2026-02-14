45f5a945de45aa19c90bbbe1cb97209688b23f8c
Justin Theriault <88461973+JustinTriesCode@users.noreply.github.com>
2026-02-13 19:57:20 -0800

    Added assets and loader file

--- MelSimpleGame/src/model/Equation.java
+++ MelSimpleGame/src/model/Equation.java
@@ -1,5 +1,17 @@
 package model;
 
+import java.util.Random;
+
 public class Equation {
+    private int value1;
+    private int value2;
+    private char operator; // +, -, *, /
+
+    public void generateEquation() {
+        value1 = new Random().nextInt(10) + 1;
+        value2 = new Random().nextInt(10) + 1;
+        operator = "+-*/".charAt(new Random().nextInt(4));
+
+    }
     // TODO: Implement logic to generate random equations and evaluate them
 }
