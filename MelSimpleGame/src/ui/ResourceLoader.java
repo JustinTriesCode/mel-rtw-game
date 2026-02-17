cd516b5686dff8d47b373deb4f25ddf7eae9dc50
Justin Theriault <88461973+JustinTriesCode@users.noreply.github.com>
2026-02-16 17:54:17 -0800

    Equationista game implemented

--- MelSimpleGame/src/ui/ResourceLoader.java
+++ MelSimpleGame/src/ui/ResourceLoader.java
@@ -9,10 +9,21 @@ import java.io.IOException;
 public class ResourceLoader {
 
     // Load an image from res/images
+    // public static BufferedImage loadImage(String fileName) {
+    // try {
+    // return ImageIO.read(new File("res/images/" + fileName));
+    // } catch (IOException e) {
+    // System.out.println("Error: Could not find image " + fileName);
+    // return null;
+    // }
+    // }
+
     public static BufferedImage loadImage(String fileName) {
         try {
-            return ImageIO.read(new File("res/images/" + fileName));
-        } catch (IOException e) {
+            // This looks inside the 'src' or 'bin' folder context
+            // Try adding a "/" at the start if it still fails: "/images/" + fileName
+            return ImageIO.read(ResourceLoader.class.getResourceAsStream("/res/images/" + fileName));
+        } catch (Exception e) {
             System.out.println("Error: Could not find image " + fileName);
             return null;
         }
@@ -25,7 +36,7 @@ public class ResourceLoader {
             return font.deriveFont(size);
         } catch (Exception e) {
             System.out.println("Error: Could not load font. Using Arial instead.");
-            return new Font("Arial", Font.BOLD, (int)size);
+            return new Font("Arial", Font.BOLD, (int) size);
         }
     }
 }
